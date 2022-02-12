c65d8d8c9b5125c9596aeaea947d77a95a77171c
Rodrigo Cavalcante <rdgcavalcante@gmail.com>
2022-02-12 10:00:44 -0300

    Acrescentado impressão da tela Rodrigo da Silva CAvalcante

--- Programa_Teste/Program.cs
+++ Programa_Teste/Program.cs
@@ -7,6 +7,7 @@ namespace Programa_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Rodrigo da Silva Cavalcante!");
         }
     }
 }
